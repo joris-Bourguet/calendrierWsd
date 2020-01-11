8fe87faef2526e6a7a5e88bc7a0c3b8260eaa528
joris-bourguet <43138838+joris-Bourguet@users.noreply.github.com>
2020-01-11 23:37:34 +0100

    Calendrier v1

--- main.js
+++ main.js
@@ -1,81 +1,28 @@
-let today = new Date();
-let currentMonth = today.getMonth();
-let currentYear = today.getFullYear();
-let selectYear = document.getElementById("year");
-let selectMonth = document.getElementById("month");
-
-let months = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
-
-let monthAndYear = document.getElementById("monthAndYear");
-showCalendar(currentMonth, currentYear);
-
-
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
+let Mois = ["rgkroj" , " bjeif"]
+let afficheMois = function (Mois) {
+    let htmlMois = ""
+    Mois.forEach(nomMois => {
+        htmlMois += nomMois
+    });
+    return htmlMois
 }
 
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
+let Heures = ["Heures :", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+let afficheHeures = function (Heures) {
+    let htmlHeures = ""
+    Heures.forEach(tranchesHeures => {
+        htmlHeures += "<td>" + tranchesHeures + "</td>"
+    });
+    return htmlHeures
 }
 
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
+let Jours = ["Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"]
+let AfficheJours = function (Jours) {
+    let htmlJours = ""
+    Jours.forEach(nomJours => {
+        htmlJours += "<th>" + nomJours + "</th>"
+    });
+    return htmlJours
 }
-
-function showCalendar(month, year) {
-
-    let firstDay = (new Date(year, month)).getDay();
-    let daysInMonth = 32 - new Date(year, month, 32).getDate();
-
-    let tbl = document.getElementById("calendar-body"); // body of the calendar
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
-
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = months[month] + " " + year;
-    selectYear.value = year;
-    selectMonth.value = month;
-
-    // creating all cells
-    let date = 1;
-    for (let i = 0; i < 6; i++) {
-        // creates a table row
-        let row = document.createElement("tr");
-
-        //creating individual cells, filing them up with data.
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            }
-            else if (date > daysInMonth) {
-                break;
-            }
-
-            else {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode(date);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.classList.add("bg-info");
-                } // color today's date
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-                date++;
-            }
-
-
-        }
-
-        tbl.appendChild(row); // appending each row into calendar body.
-    }
-
-}
+$("#Heures").html(afficheHeures(Heures))
+$("#Jours").html(AfficheJours(Jours))
