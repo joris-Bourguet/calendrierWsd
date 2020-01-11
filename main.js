2655e4a97418cf04415cba4c422905cc406402f4
joris-Bourguet <43138838+joris-Bourguet@users.noreply.github.com>
2020-01-12 00:05:52 +0100

    html+js modif

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-let Mois = ["rgkroj" , " bjeif"]
-let afficheMois = function (Mois) {
-    let htmlMois = ""
-    Mois.forEach(nomMois => {
-        htmlMois += nomMois
-    });
-    return htmlMois
-}
-
-let Heures = ["Heures :", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+let Heures = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 let afficheHeures = function (Heures) {
     let htmlHeures = ""
     Heures.forEach(tranchesHeures => {
-        htmlHeures += "<td>" + tranchesHeures + "</td>"
+        htmlHeures += "<tr> <td>" + tranchesHeures + "</td> </tr>"
     });
     return htmlHeures
 }
 
 let Jours = ["Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"]
 let AfficheJours = function (Jours) {
-    let htmlJours = ""
+    let htmlJours = "<th>Heures</th>"
     Jours.forEach(nomJours => {
         htmlJours += "<th>" + nomJours + "</th>"
     });
