cb6ed43374851062f1caf3f17dfbec06c0299140
joris-Bourguet <43138838+joris-Bourguet@users.noreply.github.com>
2020-01-12 03:04:42 +0100

    js bien avancé

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-let Heures = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-let afficheHeures = function (Heures) {
-    let htmlHeures = ""
-    Heures.forEach(tranchesHeures => {
-        htmlHeures += "<tr> <td>" + tranchesHeures + "</td> </tr>"
-    });
-    return htmlHeures
-}
+$(document).ready(function () {
+    //Creer les cellules du tableau
+    let cellulePlanning = function () {
+        // Tableau des heures
+        let Heures = ["8h", "9h", "10h", "11h", "12h", "13h", "14h", "15h", "16h", "17h", "18h"]
 
-let Jours = ["Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"]
-let AfficheJours = function (Jours) {
-    let htmlJours = "<th>Heures</th>"
-    Jours.forEach(nomJours => {
-        htmlJours += "<th>" + nomJours + "</th>"
-    });
-    return htmlJours
-}
-$("#Heures").html(afficheHeures(Heures))
-$("#Jours").html(AfficheJours(Jours))
+        //Ecrire dans les celules (tbody)
+        let htmlLigne = $("#Heures").html()
+        for (let j = 0; j < 11; j++) {
+            htmlLigne += "<tr>"
+            for (let index = 0; index < 8; index++) {
+                if (index == 0) {
+                    htmlLigne += "<td>" + Heures[j] + "</td>"
+                } else {
+                    htmlLigne += "<td> </td>"
+                }
+            }
+            htmlLigne += "</tr>"
+        }
+        return htmlLigne
+    }
+    // Creation des jours (thead)
+    let Jours = ["Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"]
+    
+
+    let AfficheJours = function (Jours, index) {
+        let htmlJours = "<th>Heures</th>"
+        let dateAjd = new Date('August 30, 1975 23:15:30')
+        let dateDuJours = new Date()
+        Jours.forEach((nomJours, index) => {
+            dateDuJours.setDate(dateAjd.getDay() + index)
+            htmlJours += "<th>" + nomJours + " " + dateDuJours.getDate() + "/" + (dateDuJours.getMonth()+1)  + " </th>"
+        });
+        return htmlJours
+    }
+    // $("#Heures").html(afficheHeures(Heures))
+    $("#Jours").html(AfficheJours(Jours))
+    $("#Heures").html(cellulePlanning())
+});
